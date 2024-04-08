016ae385b0091fd1d4997c22667c78143f463696
Milica Radovanovic <milicaa.radovanovicc@gmail.com>
2024-04-08 14:43:46 +0200

    gotov zadatak cards

--- imageCards/src/app/app.component.ts
+++ imageCards/src/app/app.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'imageCards';
+  posts = [
+    {
+      title: 'Neat tree',
+      username: 'nature',
+      imageUrl: 'assets/tree.jpeg',
+      content: 'I saw this neat tree today',
+    },
+    {
+      title: 'Mountain',
+      username: 'mountainlover',
+      imageUrl: 'assets/mountain.jpeg',
+      content: 'I saw this awesome mountain today',
+    },
+    {
+      title: 'Biking',
+      username: 'biking1222',
+      imageUrl: 'assets/biking.jpeg',
+      content: 'I have cool bike',
+    },
+  ];
 }
