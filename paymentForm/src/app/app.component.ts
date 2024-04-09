f3978eda87bbe55b624abdfbae339f181e1676ca
Milica Radovanovic <milicaa.radovanovicc@gmail.com>
2024-04-09 13:54:41 +0200

    gotov paymentForm i pipe vezba

--- paymentForm/src/app/app.component.ts
+++ paymentForm/src/app/app.component.ts
@@ -3,8 +3,32 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'paymentForm';
+  name: string = '';
+  date: string = '';
+  amount: number = 0;
+  height: number = 0;
+  miles: number = 0;
+
+  onMilesChange(event: Event) {
+    this.miles = parseFloat((event.target as HTMLInputElement).value);
+  }
+
+  onHeightChange(event: Event) {
+    this.height = parseFloat((event.target as HTMLInputElement).value);
+  }
+
+  onNameChange(event: Event) {
+    this.name = (event.target as HTMLInputElement).value;
+  }
+
+  onDateChange(event: Event) {
+    this.date = (event.target as HTMLInputElement).value;
+  }
+
+  onAmountChange(event: Event) {
+    this.amount = parseFloat((event.target as HTMLInputElement).value);
+  }
 }
--- paymentForm/src/app/app.module.ts
+++ paymentForm/src/app/app.module.ts
@@ -2,15 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ConvertPipe } from './convert.pipe';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, ConvertPipe],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
