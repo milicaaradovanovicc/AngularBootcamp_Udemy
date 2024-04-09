4aa5f5d808ef472e54d75bc0238c20a677b82fb1
Milica Radovanovic <milicaa.radovanovicc@gmail.com>
2024-04-09 18:55:49 +0200

    zadatak gotov do pola

--- pages/src/app/app.component.ts
+++ pages/src/app/app.component.ts
@@ -3,8 +3,79 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pages';
+  currentPage = 0;
+
+  images = [
+    {
+      title: 'Beautiful Tuscany',
+      url: 'https://images.unsplash.com/photo-1518098268026-4e89f1a2cd8e?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGl0YWx5fGVufDB8fDB8fHww',
+    },
+    {
+      title: 'Trevi fountain',
+      url: 'https://images.unsplash.com/photo-1525874684015-58379d421a52?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8cm9tZXxlbnwwfHwwfHx8MA%3D%3D',
+    },
+    {
+      title: 'Canals in Venice',
+      url: 'https://images.unsplash.com/photo-1523906834658-6e24ef2386f9?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dmVuaWNlfGVufDB8fDB8fHww',
+    },
+    {
+      title: 'City of Verona',
+      url: 'https://images.unsplash.com/photo-1555796861-b29396df890e?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8dmVyb25hfGVufDB8fDB8fHww',
+    },
+    {
+      title: 'Beautiful Tuscany',
+      url: 'https://images.unsplash.com/photo-1518098268026-4e89f1a2cd8e?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGl0YWx5fGVufDB8fDB8fHww',
+    },
+    {
+      title: 'Trevi fountain',
+      url: 'https://images.unsplash.com/photo-1525874684015-58379d421a52?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8cm9tZXxlbnwwfHwwfHx8MA%3D%3D',
+    },
+    {
+      title: 'Canals in Venice',
+      url: 'https://images.unsplash.com/photo-1523906834658-6e24ef2386f9?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dmVuaWNlfGVufDB8fDB8fHww',
+    },
+    {
+      title: 'City of Verona',
+      url: 'https://images.unsplash.com/photo-1555796861-b29396df890e?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8dmVyb25hfGVufDB8fDB8fHww',
+    },
+    {
+      title: 'Beautiful Tuscany',
+      url: 'https://images.unsplash.com/photo-1518098268026-4e89f1a2cd8e?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGl0YWx5fGVufDB8fDB8fHww',
+    },
+    {
+      title: 'Trevi fountain',
+      url: 'https://images.unsplash.com/photo-1525874684015-58379d421a52?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8cm9tZXxlbnwwfHwwfHx8MA%3D%3D',
+    },
+    {
+      title: 'Canals in Venice',
+      url: 'https://images.unsplash.com/photo-1523906834658-6e24ef2386f9?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dmVuaWNlfGVufDB8fDB8fHww',
+    },
+    {
+      title: 'City of Verona',
+      url: 'https://images.unsplash.com/photo-1555796861-b29396df890e?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8dmVyb25hfGVufDB8fDB8fHww',
+    },
+    {
+      title: 'Beautiful Tuscany',
+      url: 'https://images.unsplash.com/photo-1518098268026-4e89f1a2cd8e?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGl0YWx5fGVufDB8fDB8fHww',
+    },
+    {
+      title: 'Trevi fountain',
+      url: 'https://images.unsplash.com/photo-1525874684015-58379d421a52?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8cm9tZXxlbnwwfHwwfHx8MA%3D%3D',
+    },
+    {
+      title: 'Canals in Venice',
+      url: 'https://images.unsplash.com/photo-1523906834658-6e24ef2386f9?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dmVuaWNlfGVufDB8fDB8fHww',
+    },
+    {
+      title: 'City of Verona',
+      url: 'https://images.unsplash.com/photo-1555796861-b29396df890e?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8dmVyb25hfGVufDB8fDB8fHww',
+    },
+  ];
+
+  checkWindowIndex(index: number) {
+    return Math.abs(this.currentPage - index) < 5;
+  }
 }
---
+++ pages/src/app/class.directive.ts
@@ -0,0 +1,12 @@
+import { Directive, ElementRef, Input } from '@angular/core';
+
+@Directive({
+  selector: '[appClass]',
+})
+export class ClassDirective {
+  @Input() backgroundColor: string = '';
+
+  constructor(private element: ElementRef) {
+    this.element.nativeElement.style.backgroundColor = 'orange';
+  }
+}
