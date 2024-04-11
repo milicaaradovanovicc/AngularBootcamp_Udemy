344bb6ee024046ec9a94f90a7e87ef9973d0c2e3
Milica Radovanovic <milicaa.radovanovicc@gmail.com>
2024-04-11 19:35:34 +0200

    gotov modal exercise

--- comps/src/app/mods/modal/modal.component.ts
+++ comps/src/app/mods/modal/modal.component.ts
@@ -1,4 +1,10 @@
-import { Component, ElementRef, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  OnInit,
+  Output,
+} from '@angular/core';
 
 @Component({
   selector: 'app-modal',
@@ -6,9 +12,19 @@ import { Component, ElementRef, OnInit } from '@angular/core';
   styleUrls: ['./modal.component.css'],
 })
 export class ModalComponent implements OnInit {
+  @Output() close = new EventEmitter();
+
   constructor(private el: ElementRef) {}
 
   ngOnInit() {
     document.body.appendChild(this.el.nativeElement);
+  }
+
+  ngOnDestroy() {
+    this.el.nativeElement.remove();
+  }
+
+  onCloseClick() {
+    this.close.emit();
   }
 }
