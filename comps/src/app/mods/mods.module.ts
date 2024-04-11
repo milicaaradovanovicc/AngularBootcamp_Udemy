a0998920eb98d33511b9c441bfee4a22dc07b29b
Milica Radovanovic <milicaa.radovanovicc@gmail.com>
2024-04-11 15:53:55 +0200

    mods initial commit

--- comps/src/app/mods/mods.module.ts
+++ comps/src/app/mods/mods.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { ModsRoutingModule } from './mods-routing.module';
 import { ModsHomeComponent } from './mods-home/mods-home.component';
 import { SharedModule } from '../shared/shared.module';
+import { ModalComponent } from './modal/modal.component';
 
 @NgModule({
-  declarations: [ModsHomeComponent],
+  declarations: [ModsHomeComponent, ModalComponent],
   imports: [CommonModule, ModsRoutingModule, SharedModule],
   exports: [],
 })
