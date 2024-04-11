88b22d4a92c17a4675cdb1e478c4612e0bc10529
Milica Radovanovic <milicaa.radovanovicc@gmail.com>
2024-04-11 08:23:23 +0200

    Gotov zadatak Pages - custom directives

--- pages/src/app/class.directive.ts
+++ pages/src/app/class.directive.ts
@@ -4,9 +4,15 @@ import { Directive, ElementRef, Input } from '@angular/core';
   selector: '[appClass]',
 })
 export class ClassDirective {
-  @Input() backgroundColor: string = '';
+  constructor(private element: ElementRef) {}
 
-  constructor(private element: ElementRef) {
-    this.element.nativeElement.style.backgroundColor = 'orange';
+  @Input('appClass') set classNames(classObj: any) {
+    for (let key in classObj) {
+      if (classObj[key]) {
+        this.element.nativeElement.classList.add(key);
+      } else {
+        this.element.nativeElement.classList.remove(key);
+      }
+    }
   }
 }
