7347df101e78bbe7aee4262d7d6a8b24fa33e4a2
Milica Radovanovic <milicaa.radovanovicc@gmail.com>
2024-04-11 10:42:49 +0200

    added lazy loading

--- comps/src/app/app.module.ts
+++ comps/src/app/app.module.ts
@@ -3,23 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ElementsModule } from './elements/elements.module';
-import { CollectionsModule } from './collections/collections.module';
-import { ModsModule } from './mods/mods.module';
-import { ViewsModule } from './views/views.module';
+
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, NotFoundComponent],
-  imports: [
-    BrowserModule,
-    ElementsModule,
-    CollectionsModule,
-    ModsModule,
-    ViewsModule,
-    AppRoutingModule,
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
