1fa98740c67b032f867e2ecbb475100bd43680b2
Milica Radovanovic <milicaa.radovanovicc@gmail.com>
2024-04-11 14:41:41 +0200

    gotov list item

--- comps/src/app/views/views-home/views-home.component.ts
+++ comps/src/app/views/views-home/views-home.component.ts
@@ -3,8 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-views-home',
   templateUrl: './views-home.component.html',
-  styleUrls: ['./views-home.component.css']
+  styleUrls: ['./views-home.component.css'],
 })
 export class ViewsHomeComponent {
+  stats: { value: number; label: string }[] = [
+    { value: 22, label: 'Number of users' },
+    { value: 900, label: 'Revenue' },
+    { value: 50, label: 'Reviews' },
+  ];
 
+  items: { image: string; title: string; description: string }[] = [
+    {
+      image: '/assets/images/couch.jpeg',
+      title: 'Couch',
+      description: 'This is a fantastic couch!',
+    },
+    {
+      image: '/assets/images/dresser.jpeg',
+      title: 'Dresser',
+      description: 'This is an awesome dresser!',
+    },
+  ];
 }
--- comps/src/app/views/views.module.ts
+++ comps/src/app/views/views.module.ts
@@ -4,9 +4,11 @@ import { CommonModule } from '@angular/common';
 import { ViewsRoutingModule } from './views-routing.module';
 import { ViewsHomeComponent } from './views-home/views-home.component';
 import { SharedModule } from '../shared/shared.module';
+import { StatisticsComponent } from './statistics/statistics.component';
+import { ItemListComponent } from './item-list/item-list.component';
 
 @NgModule({
-  declarations: [ViewsHomeComponent],
+  declarations: [ViewsHomeComponent, StatisticsComponent, ItemListComponent],
   imports: [CommonModule, ViewsRoutingModule, SharedModule],
   exports: [],
 })
